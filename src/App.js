9abd3026b5db8015a9ab02090f010f591698391d
Abhishek Pratap Singh <abhishek@Abhisheks-MacBook-Air.local>
2023-04-11 01:01:00 +0530

    create post page created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 
 import './App.css';
-import axios from "axios";
-import { useEffect, useState } from 'react'
+import {BrowserRouter as Router,Link,Route,Routes } from 'react-router-dom';
+import Home from './pages/Home'
+import CreatePost from './pages/CreatePost'
+
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:3001/post").then((response) => {
-      setListOfPosts(response.data);
-    })
-  }, [])
+
   return (
     <div className="App">
-      {
-        listOfPosts.map((value, key) => {
-          return <div className='post'>
-          <div className='title'>{value.title}</div>
-          <div className='body'>{value.postText}</div>
-          <div className='footer'>{value.username}</div>
-        </div>
-        })}
+      <Router>
+        <Link to="/createPost">CreatePost A Post</Link>
+        <Link to="/">Home Page</Link>
+        <Routes>
+          <Route path='/' exact Component={Home}/>
+          <Route path='/createPost' exact Component={CreatePost}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
+ 
 export default App;
