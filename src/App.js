0996c7c9ebc2b1d74285a506d438dd6f2df09a0b
Abhishek Pratap Singh <abhishek@Abhisheks-MacBook-Air.local>
2023-04-13 00:10:56 +0530

    posts to open post created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router,Link,Route,Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import CreatePost from './pages/CreatePost'
+import Post from './pages/Post'
 
 
 function App() {
@@ -10,11 +11,14 @@ function App() {
   return (
     <div className="App">
       <Router>
+      <div className="navbar">
         <Link to="/createPost">CreatePost A Post</Link>
         <Link to="/">Home Page</Link>
+        </div>
         <Routes>
           <Route path='/' exact Component={Home}/>
           <Route path='/createPost' exact Component={CreatePost}/>
+          <Route path='/post/:id' exact Component={Post}/>
         </Routes>
       </Router>
     </div>
